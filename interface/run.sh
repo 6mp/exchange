d57fc54369dcd1d358d4623722a5eecc91dbb650
matt <30198937+6mp@users.noreply.github.com>
2024-04-08 16:33:20 -0400

    etup and run script

--- interface/run.sh
+++ interface/run.sh
@@ -3,4 +3,5 @@
 source venv/bin/activate
 pip install -r requirements.txt
 python3 -m grpc_tools.protoc -I../protos --python_out=src/pb --pyi_out=src/pb --grpc_python_out=src/pb ../protos/Orderbook.proto
-
+cd src
+python3 main.py
