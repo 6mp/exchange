123e8ba800e4206cca5836f96a6af68042ee487f
matt <30198937+6mp@users.noreply.github.com>
2024-04-20 13:02:56 -0400

    split files

--- engine/include/Orderbook/Orderbook.hpp
+++ engine/include/Orderbook/Orderbook.hpp
@@ -8,116 +8,49 @@
 #include <queue>
 
 class Orderbook {
+    // callbacks
+    std::function<void(Order&)> onOrderAdd;
+    std::function<void(Order&, Order&)> onOrderFill;
+    std::function<void(Order&)> onOrderAddedToBook;
+    std::function<void(Order&)> onOrderKill;
+
+    // price levels and order queue
     std::map<Price, std::deque<Order>, std::less<>> m_asks;
     std::map<Price, std::deque<Order>, std::greater<>> m_bids;
     std::queue<Order> m_orders;
+
+    // multithreading
     std::mutex m_lock;
     std::condition_variable m_condVar;
     std::atomic<bool> m_stopFlag{false};
     std::thread m_matchingThread;
 
-    std::function<void(Order&)> onOrderAddCallback;
-    std::function<void(Order&, Order&)> onOrderFillCallback;
-    std::function<void(Order&)> limitOrderAddCallback;
-    std::function<void(Order&)> marketOrderFailCallback;
-
 public:
-    Orderbook(decltype(onOrderAddCallback) onOrderAddCallback, decltype(onOrderFillCallback) onOrderFillCallback)
-        : onOrderAddCallback(std::move(onOrderAddCallback))
-        , onOrderFillCallback(std::move(onOrderFillCallback))
-        , m_matchingThread(&Orderbook::matchingThread, this) {}
+    Orderbook(
+        decltype(onOrderAdd) onOrderAdd,
+        decltype(onOrderFill) onOrderFill,
+        decltype(onOrderAddedToBook) onOrderAddedToBook,
+        decltype(onOrderKill) onOrderKill);
 
-    void addOrder(Order& order) {
-        {
-            std::lock_guard<std::mutex> lock(m_lock);
-            m_orders.push(order);
-        }
-        m_condVar.notify_one();
-        onOrderAddCallback(order);
-    }
+    ~Orderbook();
 
-    void matchingThread() {
-        while (true) {
-            std::unique_lock<std::mutex> lock(m_lock);
-            m_condVar.wait(lock, [this] { return !m_orders.empty() || m_stopFlag; });
-            if (m_stopFlag && m_orders.empty())
-                return;
+    auto addOrder(Order& order) -> void;
 
-            Order order = std::move(m_orders.front());
-            m_orders.pop();
-            lock.unlock();
+    void matchingThread();
 
-            switch (order.getType()) {
-                case OrderType::MARKET: fillMarketOrder(order); break;
-                case OrderType::LIMIT: fillLimitOrder(order); break;
-                case OrderType::INVALID: throw std::runtime_error("Invalid order type");
-            }
-        }
-    }
+    void fillMarketOrder(Order& order);
 
-    void fillMarketOrder(Order& order) {
-        switch (order.getSide()) {
-            case OrderSide::BUY: fillShit(order, m_asks); break;
-            case OrderSide::SELL: fillShit(order, m_bids); break;
-            case OrderSide::INVALID: break;
-        }
-    }
-
-    void fillLimitOrder(Order& order) {
-        switch (order.getSide()) {
-            case OrderSide::BUY: fillLimitShit(order, m_asks); break;
-            case OrderSide::SELL: fillLimitShit(order, m_bids); break;
-            case OrderSide::INVALID: break;
-        }
-    }
+    void fillLimitOrder(Order& order);
 
     template<typename Comp>
-    auto fillShit(Order& order, std::map<Price, std::deque<Order>, Comp>& priceMap) -> void {
-        for (auto it = priceMap.begin(); it != priceMap.end() && !order.isFilled(); ++it) {
-            processOrder(order, it->second);
-            if (it->second.empty())
-                priceMap.erase(it);
-        }
-        if (!order.isFilled()) {
-            marketOrderFailCallback(order);
-        }
-    }
+    auto fillShit(Order& order, std::map<Price, std::deque<Order>, Comp>& priceMap) -> void;
 
     template<typename Comp>
-    auto fillLimitShit(Order& order, std::map<Price, std::deque<Order>, Comp>& priceMap) -> void {
-        auto it = priceMap.lower_bound(order.getPrice());
-        while (it != priceMap.end() && !order.isFilled() && Comp()(order.getPrice(), it->first)) {
-            processOrder(order, it->second);
-            if (it->second.empty())
-                priceMap.erase(it++);
-            else
-                ++it;
-        }
-        if (!order.isFilled()) {
-            priceMap[order.getPrice()].push_back(order);
-            limitOrderAddCallback(order);
-        }
-    }
+    auto fillLimitShit(Order& order, std::map<Price, std::deque<Order>, Comp>& priceMap) -> void;
 
-    bool processOrder(Order& order, std::deque<Order>& orders) const {
-        while (!order.isFilled() && !orders.empty()) {
-            Order& matchOrder = orders.front();
-            order.fill(matchOrder);
-            onOrderFillCallback(order, matchOrder);
-            if (matchOrder.isFilled())
-                orders.pop_front();
-        }
-        return order.isFilled();
-    }
+    bool processOrder(Order& order, std::deque<Order>& orders) const;
 
-    void requestStop() {
-        m_stopFlag = true;
-        m_condVar.notify_one();
-        if (m_matchingThread.joinable())
-            m_matchingThread.join();
-    }
-
-    ~Orderbook() { requestStop(); }
+    void requestStop();
 };
 
 #endif    // ORDERBOOK_ORDERBOOK_HPP
---
+++ engine/src/Orderbook/Orderbook.cpp
@@ -0,0 +1,114 @@
+//
+// Created by Matthew Pallan on 4/20/24.
+//
+
+#include "Orderbook/Orderbook.hpp"
+
+#include <mutex>
+
+Orderbook::Orderbook(
+    decltype(onOrderAdd) onOrderAdd,
+    decltype(onOrderFill) onOrderFill,
+    decltype(onOrderAddedToBook) onOrderAddedToBook,
+    decltype(onOrderKill) onOrderKill)
+    : onOrderAdd(std::move(onOrderAdd))
+    , onOrderFill(std::move(onOrderFill))
+    , onOrderAddedToBook(std::move(onOrderAddedToBook))
+    , onOrderKill(std::move(onOrderKill))
+    , m_matchingThread(&Orderbook::matchingThread, this) {}
+
+Orderbook::~Orderbook() { requestStop(); }
+
+auto Orderbook::addOrder(Order& order) -> void {
+    {
+        std::scoped_lock lock{m_lock};
+        m_orders.push(order);
+    }
+    m_condVar.notify_one();
+    onOrderAdd(order);
+}
+
+void Orderbook::matchingThread() {
+    while (true) {
+
+        Order order;
+        {
+            std::unique_lock lock{m_lock};
+            m_condVar.wait(lock, [this] { return !m_orders.empty() || m_stopFlag; });
+            if (m_stopFlag && m_orders.empty())
+                return;
+
+            order = m_orders.front();
+            m_orders.pop();
+        }
+
+        switch (order.getType()) {
+            case OrderType::MARKET: fillMarketOrder(order); break;
+            case OrderType::LIMIT: fillLimitOrder(order); break;
+            case OrderType::INVALID: throw std::runtime_error("Invalid order type");
+        }
+    }
+}
+
+void Orderbook::fillMarketOrder(Order& order) {
+    switch (order.getSide()) {
+        case OrderSide::BUY: fillShit(order, m_asks); break;
+        case OrderSide::SELL: fillShit(order, m_bids); break;
+        case OrderSide::INVALID: break;
+    }
+}
+
+void Orderbook::fillLimitOrder(Order& order) {
+    switch (order.getSide()) {
+        case OrderSide::BUY: fillLimitShit(order, m_asks); break;
+        case OrderSide::SELL: fillLimitShit(order, m_bids); break;
+        case OrderSide::INVALID: break;
+    }
+}
+
+bool Orderbook::processOrder(Order& order, std::deque<Order>& orders) const {
+    while (!order.isFilled() && !orders.empty()) {
+        Order& matchOrder = orders.front();
+        order.fill(matchOrder);
+        onOrderFill(order, matchOrder);
+        if (matchOrder.isFilled())
+            orders.pop_front();
+    }
+    return order.isFilled();
+}
+
+
+template<typename Comp>
+auto Orderbook::fillShit(Order& order, std::map<Price, std::deque<Order>, Comp>& priceMap) -> void {
+    for (auto it = priceMap.begin(); it != priceMap.end() && !order.isFilled(); ++it) {
+        processOrder(order, it->second);
+        if (it->second.empty())
+            priceMap.erase(it);
+    }
+    if (!order.isFilled()) {
+        onOrderKill(order);
+    }
+}
+
+template<typename Comp>
+auto Orderbook::fillLimitShit(Order& order, std::map<Price, std::deque<Order>, Comp>& priceMap) -> void {
+    auto it = priceMap.lower_bound(order.getPrice());
+    while (it != priceMap.end() && !order.isFilled() && Comp()(order.getPrice(), it->first)) {
+        processOrder(order, it->second);
+        if (it->second.empty())
+            priceMap.erase(it++);
+        else
+            ++it;
+    }
+    if (!order.isFilled()) {
+        priceMap[order.getPrice()].push_back(order);
+        onOrderAddedToBook(order);
+    }
+}
+
+void Orderbook::requestStop() {
+    m_stopFlag = true;
+    m_condVar.notify_one();
+    if (m_matchingThread.joinable())
+        m_matchingThread.join();
+}
